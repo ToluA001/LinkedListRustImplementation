701492e344317403a61c9cc6adf2a13be2befd32
Tolu <toluwanimi611@gmail.com>
2026-06-17 15:54:01 -0500

    Changed to a single link, using RC and RefCell

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,73 @@
-use std::io;
-use std::cmp::Ordering;
-use std::collections::LinkedList;
-use rand::Rng;
+use std::rc::Rc; // reference counting, allows for mulltiple owners of a val by keeping track of how many vals point to it
+use std::cell::RefCell; // moves borrowing rules to run time, allows mutibility
 
-fn main()
-{
-    pub struct Node<'a> {
-        // "'a" is explicit lifetime
-        pub prev: Option<&'a Box<i32>>, // pointer to some val on the heap via a refrence so ownership isn't transfered
-        // rust doesnt have null vals so how will we say that a pointer is null? option enum
-        // we're saying prev can be either, Nothing(None) or Something(Some)
-        pub val: Box<i32>,
-        pub next: Option<&'a Box<i32>>,
 
-    }
-    impl Node<'_>{
-        // associated functions, all fn within an impl
-        // fn with &self as a perameter are methods
+pub struct Node {
+    // "'a" is explicit lifetime, meaning the struct can only live as long as the lifetime of the reference
 
-        /// Makes a new node
-        fn new (val:Box<i32>) -> Self{
-            Self{
-                prev: None,
-                val,
-                next: None,
-            }
+    // rust doesnt have null vals so how will we say that a pointer is null? "Option" enum
+    // we're saying prev can be either, Nothing(None) or Something(Some)
+
+    pub val: Option<Box<i32>>,
+    pub next: Option<Rc<RefCell<Node>>>, // pointer to some val on the heap via refrence counting so ownership isn't transfered
+}
+impl Node<>{
+    // associated functions, all fn within an impl
+    // fn with &self as a perameter are methods
+
+    /// Makes a new node
+    fn new (val:Option<Box<i32>>) -> Self{
+        // Constructor
+        Self{
+            val, // value is a ppointer to a val on the heap, may or may not be null(NONE)
+            next: None,
         }
-
-    }
-    pub struct LinkedList<'a> {
-        head: Node<'a>,
-        tail: Node<'a>,
-        size: u32,
-        capacity: u32
     }
 
+    // rust does not allow for function overloading :(
+    /// Makes a new empty node
+      fn create_empty() -> Self{
+         Self{
+             val: None,
+             next: None,
+         }
+      }
 
-    fn main(){
-        let mut mock_head = Node::new(Box::new(89));
+}
+pub struct LinkeddList {
+    head: Node,
+    tail: Node,
+    size: u32,
+    capacity: u32
+}
+impl LinkeddList<> {
+    // fn new(_head:Node, _tail:Node) -> Self{
+    //     // constructor that takes in a head and a tail and returns a linked list
+    //     Self {
+    //         head: _head,
+    //         tail: _tail,
+    //         size: 1,
+    //         capacity: 1,
+    //     }
+    // }
+}
+fn main() {
+    // the below is a mock manually created linked list
+    let mut mock_head = Node::new(Option::from(Box::new(9)));
+    let mut mock_tail = Node::new(Option::from(Box::new(19)));
 
-        let mut mock_tail = Node::new(Box::new(19));
+    let node_pointer_1 = Option::from(Rc::new(RefCell::new(mock_head))); // moves the value of mock_head into the Rc
+    let node_pointer_2 = Option::from(Rc::new(RefCell::new(mock_tail)));
 
-        mock_head.prev = None;
-        mock_head.next = Option::from(&mock_tail.val);
-
-
-        mock_tail.prev = Option::from(&mock_tail.val);
-        mock_tail.next = Option::None;
-
+    match &node_pointer_1 {
+        Some(node) => {
+            node.borrow_mut().next = node_pointer_2;
+        },
+        None => {
+            println!("Node is None");
+        }
     }
-
+    
+    println!("{:?}",node_pointer_1.clone().unwrap().borrow_mut().val);
+    println!("{:?}",node_pointer_1.clone().unwrap().borrow_mut().val);
 }
